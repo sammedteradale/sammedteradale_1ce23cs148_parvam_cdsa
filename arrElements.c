17a95016ce3ba5e03f7895daebf2d2600826157d
sammedteradale <sammedteradale2004@gmail.com>
2024-11-08 12:39:07 +0530

    Add files via upload

---
+++ arrElements.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+ printf("enter the no of elements");
+ scanf("%d",&n);
+ int arr[n];
+ printf("enter 5 elements");
+ for(int i=0; i<n; i++){
+    scanf("%d",&arr[i]);
+ }   
+ printf("the arr elements are....");
+ for(int i=0; i<n; i++){
+    printf("%d",arr[i]);
+ }
+ return 0;
+}
---
+++ sumofarryyyyyy.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8};
+    for(int i=0;i<=6;i++){
+    printf("%d\n",arr[i]+5);
+    }
+    return 0;
+}
